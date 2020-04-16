feab99e14ca912d57a457c887b5c37214810dde2
ynchu <3319034446@qq.com>
2020-04-16 09:44:29 +0800

    修改README和书籍实体类

--- src/main/java/com/nchu/recom/domain/Book.java
+++ src/main/java/com/nchu/recom/domain/Book.java
@@ -2,6 +2,8 @@ package com.nchu.recom.domain;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.stereotype.Component;
 
 /**
@@ -11,31 +13,132 @@ import org.springframework.stereotype.Component;
 @ApiModel(description = "图书实体类")
 public class Book {
     @ApiModelProperty(notes = "图书编号")
-    private int bookId;
+    @Id
+    @Column(value = "id")
+    private int id;
 
     @ApiModelProperty(notes = "图书名称")
-    private String bookName;
+    @Column(value = "name")
+    private String name;
 
-    @ApiModelProperty(notes = "封面路径")
-    private String cover;
+    @ApiModelProperty(notes = "图书出版社")
+    @Column(value = "publish")
+    private String publish;
+
+    @ApiModelProperty(notes = "图书isbn")
+    @Column(value = "isbn")
+    private String isbn;
+
+    @ApiModelProperty(notes = "图书作者")
+    @Column(value = "writer")
+    private String writer;
+
+    @ApiModelProperty(notes = "图书本地链接")
+    @Column(value = "local_url")
+    private String localUrl;
+
+    @ApiModelProperty(notes = "图书价格")
+    @Column(value = "price")
+    private float price;
 
     @ApiModelProperty(notes = "图书类型")
+    @Column(value = "type")
     private String type;
 
-    public int getBookId() {
-        return bookId;
+    @ApiModelProperty(notes = "图书上传人")
+    @Column(value = "upper")
+    private String upper;
+
+    @ApiModelProperty(notes = "图书类型")
+    @Column(value = "viewed")
+    private int viewed;
+
+    @ApiModelProperty(notes = "图书封面")
+    @Column(value = "cover")
+    private String cover = "images/default.png";
+
+    @ApiModelProperty(notes = "图书状态")
+    @Column(value = "status")
+    private int status;
+
+    public int getId() {
+        return id;
     }
 
-    public void setBookId(int bookId) {
-        this.bookId = bookId;
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public String getBookName() {
-        return bookName;
+    public String getName() {
+        return name;
     }
 
-    public void setBookName(String bookName) {
-        this.bookName = bookName;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPublish() {
+        return publish;
+    }
+
+    public void setPublish(String publish) {
+        this.publish = publish;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public String getLocalUrl() {
+        return localUrl;
+    }
+
+    public void setLocalUrl(String localUrl) {
+        this.localUrl = localUrl;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUpper() {
+        return upper;
+    }
+
+    public void setUpper(String upper) {
+        this.upper = upper;
+    }
+
+    public int getViewed() {
+        return viewed;
+    }
+
+    public void setViewed(int viewed) {
+        this.viewed = viewed;
     }
 
     public String getCover() {
@@ -46,11 +149,11 @@ public class Book {
         this.cover = cover;
     }
 
-    public String getType() {
-        return type;
+    public int getStatus() {
+        return status;
     }
 
-    public void setType(String type) {
-        this.type = type;
+    public void setStatus(int status) {
+        this.status = status;
     }
 }
