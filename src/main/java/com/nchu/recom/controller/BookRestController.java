f3b347c6346793d356f522feff4fd76dd80f3757
ynchu <3319034446@qq.com>
2020-05-18 08:54:55 +0800

    修复少量bug

--- src/main/java/com/nchu/recom/controller/BookRestController.java
+++ src/main/java/com/nchu/recom/controller/BookRestController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 @Api("This is a RESTful Controller Demo")
 @CrossOrigin
@@ -32,15 +33,30 @@ public class BookRestController {
         return new ResponseEntity<Collection<Book>>(books, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/insert",method = RequestMethod.POST,produces = "application/json")
-    public ResponseEntity<Book> bookAdd(@RequestBody Book book){
+    @RequestMapping(value = "filter", method = RequestMethod.GET, produces = "application/json")
+    public ResponseEntity<Collection<Book>> getBooksFilter(String name) {
+        Collection<Book> books;
+        if (name == null) {
+            System.out.println("查询所有书籍");
+            books = bookService.getAllBooks();
+        } else {
+            System.out.println("根据名称查询书籍");
+            books = bookService.findByName(name);
+        }
+        Collection<Book> booksFilter;
+        booksFilter = books.stream().filter(book -> book.getStatus() == 0).collect(Collectors.toList());
+        return new ResponseEntity<Collection<Book>>(booksFilter, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/insert", method = RequestMethod.POST, produces = "application/json")
+    public ResponseEntity<Book> bookAdd(@RequestBody Book book) {
         System.out.println("新书添加，书名:" + book.getName());
         bookService.addBook(book);
         return new ResponseEntity<Book>(HttpStatus.ACCEPTED);
     }
 
-    @RequestMapping(value = "/delete",method = RequestMethod.DELETE)
-    public ResponseEntity<Collection<Book>> bookDel(@RequestParam("id") int id){
+    @RequestMapping(value = "/delete", method = RequestMethod.DELETE)
+    public ResponseEntity<Collection<Book>> bookDel(@RequestParam("id") int id) {
         System.out.println("删除书，书号" + id);
         bookService.delBook(id);
         return new ResponseEntity<Collection<Book>>(HttpStatus.OK);
